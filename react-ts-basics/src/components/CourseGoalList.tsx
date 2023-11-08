72e02abf398dd7bdc7f1a674488c1353bb7373b1
Eduardo S. Libardi <eslibardi@gmail.com>
2023-11-08 12:36:50 -0300

    Components with Discriminated Unions
    Flexible components with required props combinations

--- react-ts-basics/src/components/CourseGoalList.tsx
+++ react-ts-basics/src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
+import { type ReactNode } from 'react';
 import CourseGoal from './CourseGoal';
 import { type CourseGoal as CGoal } from '../App';
+import InfoBox from './InfoBox';
 
 type CourseGoalListProps = {
   goals: CGoal[];
@@ -7,18 +9,39 @@ type CourseGoalListProps = {
 };
 
 const CourseGoalList = ({ goals, onDelete }: CourseGoalListProps) => {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode='hint'>
+        You have no course goals yet. Start adding some!
+      </InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode='warning' severity='medium'>
+        You're collecting a lot of goals. Don't put too much on your plate!
+      </InfoBox>
+    );
+  }
+
   return (
-    <ul>
-      {goals.map((goal) => {
-        return (
-          <li key={goal.id}>
-            <CourseGoal title={goal.title} onDelete={onDelete} id={goal.id}>
-              <p>{goal.description}</p>
-            </CourseGoal>
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => {
+          return (
+            <li key={goal.id}>
+              <CourseGoal title={goal.title} onDelete={onDelete} id={goal.id}>
+                <p>{goal.description}</p>
+              </CourseGoal>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 };
 export default CourseGoalList;
