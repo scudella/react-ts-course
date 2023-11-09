a40193efc9626185ddef691a1bd33fbe47a01ffe
Eduardo S. Libardi <eslibardi@gmail.com>
2023-11-09 14:48:49 -0300

    Custom Form component
    Avoid type casting with as -> using type guard
    Exposing component API with useImperativeHandle

--- 04-New-Project-Starting-Code/src/App.tsx
+++ 04-New-Project-Starting-Code/src/App.tsx
@@ -1,22 +1,46 @@
-import Input from './components/Input';
+// import Input from './components/Input';
+import Form, { type FormHandle } from './components/Form';
 import InputWithForwardRef from './components/InputWithForwardRef';
 import Button from './components/Button';
-import Container from './components/Container';
+// import Container from './components/Container';
 import { useRef } from 'react';
 
 function App() {
-  const input = useRef<HTMLInputElement>(null);
+  const customForm = useRef<FormHandle>(null);
+  // const input = useRef<HTMLInputElement>(null);
+
+  function handleSave(data: unknown) {
+    // Replace type casting (type assertion) by type guard
+    // const extractedData = data as { name: string; age: string };
+    // console.log(extractedData);
+
+    if (
+      !data ||
+      typeof data !== 'object' ||
+      !('name' in data) ||
+      !('age' in data)
+    ) {
+      return;
+    }
+
+    // at this point, TypeScript knows that data MUST BE an object
+    // with a name and age property
+    // otherwise, the previous if statement would have returned
+    customForm.current?.clear();
+  }
 
   return (
     <main>
-      <Input id='name' label='Your name' type='text' />
-      <Input id='age' label='Your age' type='number' />
-      <p>
-        <Button>A Button</Button>
-      </p>
-      <p>
+      <Form onSave={handleSave} ref={customForm}>
+        <InputWithForwardRef type='text' label='Name' id='name' />
+        <InputWithForwardRef type='number' label='Age' id='age' />
+        <p>
+          <Button>Save</Button>
+        </p>
+      </Form>
+      {/* <p>
         <Button href='https://google.com'>A Link</Button>
-      </p>
+      </p> */}
       {/* <p>
         <Button el='button'>A Button</Button>
       </p>
@@ -25,10 +49,10 @@ function App() {
           A Link
         </Button>
       </p> */}
-      <Container as={Button} onClick={() => {}} type='button'>
+      {/* <Container as={Button} onClick={() => {}} type='button'>
         Click Me
       </Container>
-      <InputWithForwardRef label='Test' id='test' ref={input} />
+      <InputWithForwardRef label='Test' id='test' ref={input} /> */}
     </main>
   );
 }
