a10f48d73c2186572f13d2ea3264c7473e7dc61a
Eduardo S. Libardi <eslibardi@gmail.com>
2023-11-10 14:20:01 -0300

    Setup context

---
+++ 05-context-api-useReducer/src/store/timers-context.tsx
@@ -0,0 +1,46 @@
+import { type ReactNode, createContext, useContext } from 'react';
+
+type Timer = {
+  name: string;
+  duration: number;
+};
+
+type TimersState = {
+  isRunning: boolean;
+  timers: Timer[];
+};
+
+type TimersContextValue = TimersState & {
+  addTimer: (timerData: Timer) => void;
+  startTimers: () => void;
+  stopTimers: () => void;
+};
+
+const TimersContext = createContext<TimersContextValue | null>(null);
+
+export function useTimersContext() {
+  const timersCtx = useContext(TimersContext);
+  if (timersCtx === null) {
+    throw new Error('TimersContext is null');
+  }
+  return timersCtx;
+}
+
+type TimersContextProviderProps = {
+  children: ReactNode;
+};
+
+export default function TimersContextProvider({
+  children,
+}: TimersContextProviderProps) {
+  const ctx: TimersContextValue = {
+    timers: [],
+    isRunning: true,
+    addTimer(timerData) {},
+    startTimers() {},
+    stopTimers() {},
+  };
+  return (
+    <TimersContext.Provider value={ctx}>{children}</TimersContext.Provider>
+  );
+}
