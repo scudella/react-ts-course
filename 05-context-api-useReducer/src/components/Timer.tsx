c3262c774939023a3e25e9871742cfa565862e5a
Eduardo S. Libardi <eslibardi@gmail.com>
2023-11-11 15:56:18 -0300

    useEffect to decrease the timer
    start / stop the timers

--- 05-context-api-useReducer/src/components/Timer.tsx
+++ 05-context-api-useReducer/src/components/Timer.tsx
@@ -1,11 +1,47 @@
 import Container from './UI/Container.tsx';
-import { type Timer as TimerProps } from '../store/timers-context.tsx';
+import {
+  useTimersContext,
+  type Timer as TimerProps,
+} from '../store/timers-context.tsx';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Timer({ name, duration }: TimerProps) {
+  const interval = useRef<ReturnType<typeof setInterval> | null>(null);
+  const [remainingTime, setRemainingTime] = useState(duration * 1000);
+  const { isRunning } = useTimersContext();
+
+  if (remainingTime <= 0 && interval.current) {
+    clearInterval(interval.current);
+  }
+
+  useEffect(() => {
+    let timer: ReturnType<typeof setInterval>;
+    if (isRunning) {
+      timer = setInterval(() => {
+        setRemainingTime((prevTime) => {
+          if (prevTime <= 0) {
+            return prevTime;
+          }
+          return prevTime - 50;
+        });
+      }, 50);
+      interval.current = timer;
+    } else if (interval.current) {
+      clearInterval(interval.current);
+    }
+
+    return () => clearInterval(timer);
+  }, [isRunning]);
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
   return (
     <Container as='article'>
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p>
+        <progress max={duration * 1000} value={remainingTime} />
+      </p>
+      <p>{formattedRemainingTime}</p>
     </Container>
   );
 }
